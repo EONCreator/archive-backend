24c14b06cf7d2698bf3a8a86ac54b3b913370726
Islam Dortkuliyev <96120052+EONCreator@users.noreply.github.com>
2024-11-06 03:25:01 +0300

    Update WebConfig.java

--- src/main/java/com/archiveapp/demo/WebConfig.java
+++ src/main/java/com/archiveapp/demo/WebConfig.java
@@ -9,10 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Укажите путь, для которого разрешен CORS
-                .allowedOrigins("http://localhost:5173") // Разрешить доступ только с этого источника
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Разрешенные методы
-                .allowedHeaders("*") // Разрешенные заголовки
-                .allowCredentials(true); // Если необходимо разрешить куки
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
